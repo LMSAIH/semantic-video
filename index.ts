f8b8875968b301968e0ab6084de3282e5df10dac
Axel Velasquez <yoloax11@gmail.com>
2025-12-23 19:27:07 -0600

    Add index.ts to export core functionalities and utility modules

---
+++ index.ts
@@ -0,0 +1,28 @@
+// Frame analysis
+export { analyzeFrame, analyzeFrames } from './src/frame-analyzers';
+
+// Token estimation
+export { estimateFrameTokens, estimateFramesTokens, countTextTokens, countImageTokens, type TokenEstimate } from './src/token-estimate';
+
+// Models configuration
+export { 
+  VISION_MODELS, 
+  getModelConfig, 
+  getModelPricing, 
+  getImageTokenMultiplier, 
+  calculateCost,
+  getSupportedModels,
+  type VisionModelConfig, 
+  type ModelPricing 
+} from './src/models';
+
+// Core classes
+export { default as SemanticVideo, type FrameData } from './src/semantic-video';
+export { default as SemanticVideoClient, type VideoAnalysisResult, type VideoTokenEstimate, type MultiVideoTokenEstimate } from './src/client';
+export { default } from './src/client';
+
+// Utility modules (for advanced usage)
+export { default as VideoRegistry } from './src/video-registry';
+export { default as StatsTracker, type ClientStats } from './src/stats-tracker';
+export { default as TokenEstimator } from './src/token-estimator';
+export { default as VideoSearch, type SearchResult } from './src/video-search';
